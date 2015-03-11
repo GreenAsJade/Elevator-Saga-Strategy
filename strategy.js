78f1066d45ec62c95a61acfe08f3bfe3d02f279a
Yee Cheng Chin <ychin.git@gmail.com>
2015-03-11 04:31:37 -0700

    Made scheduling a little better, but using compare operators instead of scores so I can do more nuanced comparisons. Now also prioritize "on the way" cars to pick up passengers more than idle ones, and for idle ones will calculate the one with the closest distance.

--- strategy.js
+++ strategy.js
@@ -115,6 +115,7 @@
             })(elevator);
         };
 
+        /*
         function TryScheduleElevator(elevator, dir, floorNum) {
             var score = 0;
 
@@ -124,21 +125,49 @@
                 score = 10; // always schedule idle first. TODO maybe compare with distance? need to worry about other elevators overflowing though
             }
             else if (floorDir == dir && elevator.currentDir == dir) {
-                score = 5; // on the way
+                score = 15; // on the way, TODO should this have highest priority?
             }
             else { // TODO add not on the way, BUT last stop
                 score = 1;
             }
             return score;
         }
+        */
+        function CompareElevatorsForScheduling(elevator1, elevator2, dir, floorNum) {
+            // returns true means elevator2 is better
+            var score = 0;
+
+            var floorDir1 = floorNum > elevator1.currentFloor() ? 1 : (floorNum < elevator1.currentFloor() ? -1 : 0);
+            var floorDir2 = floorNum > elevator2.currentFloor() ? 1 : (floorNum < elevator2.currentFloor() ? -1 : 0);
+
+            var onTheWay1 = (floorDir1 == dir && elevator1.currentDir == dir);
+            var onTheWay2 = (floorDir2 == dir && elevator2.currentDir == dir);
+
+            if (onTheWay2) {
+                if (!onTheWay1)
+                    return true;
+            }
+            else {
+                if (elevator2.currentDir == 0) {
+                    if (elevator1.currentDir != 0)
+                        return true; // try to schedule idle
+
+                    return Math.abs(elevator2.currentFloor() - floorNum) < Math.abs(elevator1.currentFloor() - floorNum);
+                }
+            }
+            return false;
+        }
 
         function ScheduleBestElevator(dir, floorNum) {
             var curScore = 0;
             var curBestElevator = elevators[0];
-            for (var i = 0; i < numElevators; i++) { // TODO add some randomness? since we re not accounting for distance etc, random helps mix up which elevator to pick. Can make starting index random plus tie break random
-                var score = TryScheduleElevator(elevators[i], 1, curFloorNum);
-                if (score > curScore) {
-                    curScore = score;
+            for (var i = 1; i < numElevators; i++) { // TODO add some randomness? since we re not accounting for distance etc, random helps mix up which elevator to pick. Can make starting index random plus tie break random
+                //var score = TryScheduleElevator(elevators[i], 1, curFloorNum);
+                //if (score > curScore) {
+                    //curScore = score;
+                    //curBestElevator = elevators[i];
+                //}
+                if (CompareElevatorsForScheduling(curBestElevator, elevators[i], 1, curFloorNum)) {
                     curBestElevator = elevators[i];
                 }
             }
@@ -147,7 +176,7 @@
 
         function ScheduleElevator(elevator, dir, floorNum) {
             elevator.floorPressed[floorNum] = true;
-            SetupElevatorDestinations(elevator);
+            SetupElevatorDestinations(elevator); // TODO refactor this. If idle we should go all the way to the top and go down
         }
 
         for (var curFloorNum = 0; curFloorNum < numFloors; curFloorNum++) {
